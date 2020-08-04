93359f2c6801f00a47ef4afdce242d65bf98cfdf
Kyle Smith <ksmith@smc3.com>
2020-08-04 15:40:07 -0400

    modify sys.path for testing

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,11 @@
+# add the project directory to the pythonpath
+import os.path
+import sys
+from pathlib import Path
+dir_ = Path(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, str(dir_.parent))
+
+
 import asyncio
 import pytest
 
