82acde5f69d17ed8ea4ceffdb70beaf59ec6dd1f
Kyle Smith <ksmith@smc3.com>
2021-11-02 10:18:17 -0400

    fix workflow

--- quart_events/__init__.py
+++ quart_events/__init__.py
@@ -1,9 +1,9 @@
 import sys
 
 
-__VERSION__ = '0.3.4-dev'
-__DATE__ = '2021-05-28'
-__MIN_PYTHON__ = (3, 7)
+__VERSION__ = '0.4.0-dev'
+__DATE__ = '2021-10-02'
+__MIN_PYTHON__ = (3, 8)
 
 
 if sys.version_info < __MIN_PYTHON__:
@@ -12,3 +12,4 @@ if sys.version_info < __MIN_PYTHON__:
 
 from .broker import EventBroker
 from .errors import EventBrokerError, EventBrokerAuthError
+
